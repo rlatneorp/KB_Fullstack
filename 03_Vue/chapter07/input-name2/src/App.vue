3afc3f2e9e9605de5d40758cc17c199460e5b597
rlatneorp <rlatneorp@gmail.com>
2024-05-28 16:59:08 +0900

    feat : 일부러 오류생기게 해서 유효성 검사

---
+++ 03_Vue/chapter07/input-name2/src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <!-- 자식 컴포넌트에서 정의한 nameChanged 이벤트를 nameChangedHandler 함수와 연결 -->
+    <!-- (사용자 정의 이벤트) -->
+    <inputName @nameChanged="nameChangedHandler" />
+    <br />
+    <h3>App 데이터 : {{ parentName }}</h3>
+  </div>
+</template>
+<script>
+import InputName from './components/InputName.vue';
+
+export default {
+  name: 'App',
+  components: {
+    InputName,
+  },
+  data() {
+    return {
+      parentName: '',
+    };
+  },
+  methods: {
+    nameChangedHandler(e) {
+      // 받아온 객체 안에 있는 name을 접근해서 data에 있는 parentName에 넣어줌
+      this.parentName = e.name;
+    },
+  },
+};
+</script>
---
+++ 03_Vue/chapter07/input-name2/src/components/InputName.vue
@@ -0,0 +1,28 @@
+<template>
+  <div style="border: solid 1px gray; padding: 5px">
+    <!-- name 속성을 input과 양방향 바인딩시킴 -->
+    이름 : <input type="text" v-model="name" />
+    <!-- 버튼 클릭시 nameChanged라는 이벤트에 name 값을 객체로 담아서 부모에게 전송 -->
+    <button @click="$emit('nameChanged', { name })">이벤트 발신</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'InputName',
+  // nameChaged1이 아닌 이벤트가 발생했으므로 경고창 출력
+  // emits: ['nameChaged1'],
+
+  emits: {
+    nameChanged: (e) => {
+      // name이 비어있지 않고, type이 string이며, 앞뒤 공백을 제거하고 길이가 3 이상일 경우 true 리턴
+      return e.name && typeof e.name === 'String' && e.name.trim().length >= 3
+        ? true
+        : false;
+    },
+  },
+  data() {
+    return { name: '' };
+  },
+};
+</script>
