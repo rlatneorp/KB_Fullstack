fc652287dc4b699c3840617edf959411b3dd321b
rlatneorp <rlatneorp@gmail.com>
2024-05-29 16:13:32 +0900

    feat : scoped 지역 css 적용하기

---
+++ 03_Vue/chapter08/style-test2/src/components/Child1.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="child">
+    <h2>Child1</h2>
+  </div>
+</template>
+<script>
+export default {
+  name: 'Child1',
+};
+</script>
+<!-- 해당 컴포넌트에서만 적용하려면 scoped를 붙여준다 -->
+<style scoped>
+.child {
+  background-color: yellow;
+  border: solid 1px black;
+  margin: 1.5em;
+  padding: 1em;
+}
+</style>
---
+++ 03_Vue/chapter08/style-test2/src/components/Child3.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="child">
+    <h2>Child3</h2>
+  </div>
+</template>
+<script>
+export default {
+  name: 'Child3',
+};
+</script>
+<style scoped>
+.child {
+  background-color: orange;
+  border: solid 1px black;
+  margin: 1.5em;
+  padding: 1em;
+}
+</style>
