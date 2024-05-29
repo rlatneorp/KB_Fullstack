83e46fa4e08668870e0e52120bd9d544296fbf98
rlatneorp <rlatneorp@gmail.com>
2024-05-29 15:48:42 +0900

    feat : component 예시

---
+++ 03_Vue/chapter08/style-test/src/App.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <Child1 />
+  </div>
+</template>
+<script>
+import Child1 from './components/Child1.vue';
+
+export default {
+  name: 'App',
+  components: { Child1 },
+};
+</script>
