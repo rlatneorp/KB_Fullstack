0edf265d5b9cae86c027aa317f66a3cfe5d27c34
rlatneorp <rlatneorp@gmail.com>
2024-05-29 16:20:10 +0900

    feat : moudle로 리팩토링

---
+++ 03_Vue/chapter08/style-test3/src/components/Child2.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="child">
+    <h2>Child2</h2>
+  </div>
+</template>
+<script>
+export default {
+  name: 'Child2',
+};
+</script>
+<style scoped>
+.child {
+  background-color: blue;
+  border: solid 1px black;
+  margin: 1.5em;
+  padding: 1em;
+}
+</style>
---
+++ 03_Vue/chapter08/style-test3/src/components/Child3.vue
@@ -0,0 +1,22 @@
+<template>
+  <!-- 모듈로 등록한 스타일은 $style내에 저장됨 -->
+  <div :class="$style.child">
+    <h2>Child3</h2>
+  </div>
+</template>
+<script>
+export default {
+  name: 'Child3',
+  created() {
+    console.log(this.$style);
+  },
+};
+</script>
+<style module>
+.child {
+  background-color: orange;
+  border: solid 1px black;
+  margin: 1.5em;
+  padding: 1em;
+}
+</style>
