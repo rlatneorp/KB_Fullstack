da81df6356c9c5d38ff48babed2ea3ed20a94e5d
rlatneorp <rlatneorp@gmail.com>
2024-05-30 17:52:34 +0900

    feat : v-model 예시

---
+++ 03_Vue/chapter08/dynamic-component-test2/src/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <!-- 주고받고 하는 과정을 아래코드로 생략가능 -->
+    <InputName v-model:name="searchName" />
+    <h2>검색어 : {{ searchName }}</h2>
+  </div>
+</template>
+<script>
+import InputName from './components/InputName.vue';
+
+export default {
+  name: 'App',
+  components: { InputName },
+  data() {
+    return {
+      searchName: 'John',
+    };
+  },
+  methods: {
+    // changeSearchName은 v-model로 명명한 바인딩시 생략 가능
+    changeSearchName(name) {
+      console.log(name);
+      this.searchName = name;
+    },
+  },
+};
+</script>
+<style>
+.header {
+  padding: 20px 0px 0px 20px;
+}
+.headerText {
+  padding: 0px 20px 40px 20px;
+}
+.tab {
+  padding: 30px;
+}
+</style>
---
+++ 03_Vue/chapter08/dynamic-component-test2/src/components/InputName.vue
@@ -0,0 +1,15 @@
+<template>
+  <!-- 부모한테 받아온 props인 name을 value 값으로 지정 -->
+  <!-- input입력시 input의 value값을 unpate-name 이벤트로 부모에 넘겨줌 -->
+  <input
+    type="text"
+    :value="name"
+    @input="$emit('update-name', $event.target.value)"
+  />
+</template>
+<script>
+export default {
+  name: 'InputName',
+  props: ['name'],
+};
+</script>
