8dfec9139c9c4022c79402703b5fe99e2aca8e71
rlatneorp <rlatneorp@gmail.com>
2024-05-30 12:40:54 +0900

    feat : vue.js setup 사용하기

---
+++ 03_Vue/chapter09/calc-component-test/src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <Calc2 />
+  </div>
+</template>
+
+<script>
+// import Calc from './components/Calc.vue';
+import Calc2 from './components/Calc2.vue';
+
+export default {
+  name: 'App',
+  components: { Calc2 },
+};
+</script>
---
+++ 03_Vue/chapter09/calc-component-test/src/components/Calc2.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    X : <input type="text" v-model.number="x" /> <br />
+    Y : <input type="text" v-model.number="y" /> <br />
+    <button @click="calcAdd">계산</button><br />
+    <div>결과 : {{ result }}</div>
+  </div>
+</template>
+
+<!-- composition API 내의 함수를 사용하려면 import필수 -->
+<script>
+import { ref } from 'vue'; // ref : 반응형 데이터
+
+export default {
+  name: 'Calc',
+  setup() {
+    // ref로 값형 데이터를 감싸줘야 반응형으로 동작한다
+    // 기존 옵션 API의 data와 같은 역할
+    const x = ref(10);
+    const y = ref(20);
+    const result = ref(30);
+
+    const calcAdd = () => {
+      result.value = x.value + y.value;
+    };
+    // return을 해줘야 template 쪽에서 사용 가능
+    return { x, y, result, calcAdd };
+  },
+};
+</script>
