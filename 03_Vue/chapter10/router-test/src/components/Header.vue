0e043749d794d5b1d4a77e749feaf2c5d0466616
rlatneorp <rlatneorp@gmail.com>
2024-05-31 14:45:45 +0900

    feat : router 예시

---
+++ 03_Vue/chapter10/router-test/src/components/Header.vue
@@ -0,0 +1,48 @@
+<template>
+  <nav class="navbar navbar-expand-md bg-dark navbar-dark mt-2">
+    <span class="navbar -brand">비전공D(classD)</span>
+    <button class="navbar -toggler" type="button" @click="changeIsNavShow">
+      <span class="navbar -toggler -icon"></span>
+    </button>
+    <div :class="navClass">
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <router-link-link class="nav-link" to="/"> 홈</router-link-link>
+        </li>
+        <li class="nav-item">
+          <router-link-link class="nav-link" to="/about">소개</router-link-link>
+        </li>
+        <li class="nav-item">
+          <router-link-link class="nav-link" to="/members">
+            멤버
+          </router-link-link>
+        </li>
+        <li class="nav-item">
+          <router-link-link class="nav-link" to="/videos">
+            영상
+          </router-link-link>
+        </li>
+      </ul>
+    </div>
+  </nav>
+</template>
+<script>
+import { reactive, computed } from 'vue';
+export default {
+  setup() {
+    const state = reactive({ isNavShow: false });
+    // navClass에 state의 isNavShow가 ture면 앞의 클래스명을 넣고 false면 뒤의 클래스명을 넣는다
+    const navClass = computed(() =>
+      state.isNavShow
+        ? 'collapse navbar-collapse show'
+        : 'collapse navbar-collapse'
+    );
+    // 버튼 클릭시 sate의 isNavShow를 ㅂ나대로 바꿔줌(true-false, false->true)
+    const changeIsNavShow = () => {
+      state.isNavShow = !state.isNavShow;
+    };
+    // script setup 사용시 return 필요없음
+    return { state, changeIsNavShow, navClass };
+  },
+};
+</script>
---
+++ 03_Vue/chapter10/router-test/src/router/index.js
@@ -0,0 +1,32 @@
+import { createRouter, createWebHistory } from 'vue-router';
+// ../아 @/ 모두 src 폴더를 나타냄
+import Home from '@/pages/Home.vue';
+import About from '@/pages/About.vue';
+import Members from '@/pages/Members.vue';
+import Videos from '@/pages/Videos.vue';
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes: [
+    {
+      path: '/',
+      name: 'home', // name 속성은 필 수가 아님
+      // 무조건 로딩되어야 하는 페이지가 있기 때문에 정적으로 처리
+      component: Home,
+    },
+    {
+      path: '/about',
+      name: 'about',
+      // 해당 경로가 방문됐을때만 로딩되게 만들어둠, 화살표함수로 처리
+      // component: () => import('../views/AboutView.vue'),
+      component: About,
+    },
+    {
+      path: '/members',
+      component: Members,
+    },
+  ],
+});
+
+// router 만들고 내보내기
+export default router;
