618a2acbae721651e3b57e22f5117a457cf3829a
rlatneorp <rlatneorp@gmail.com>
2024-06-03 17:43:28 +0900

    comment : 주석 처리

--- 03_Vue/chapter11/axios-test-app/src/App5.vue
+++ 03_Vue/chapter11/axios-test-app/src/App5.vue
@@ -12,11 +12,14 @@ const todoUrlPrefix = '/api/todos/';
 
 const requestAPI = async () => {
   const url = '/api/todos';
+  // API 요청에 성공했을때
   try {
     const response = await axios.get(url, { timeout: 900 });
     console.log('# 응답객체 : ', response);
+    // API 요청에 실패했을때
   } catch (e) {
     console.log('## 다음 오류가 발생했습니다.');
+    // instanceof: e가 error 파일인지 체크
     if (e instanceof Error) console.log(e.message);
     else console.log(e);
   }
