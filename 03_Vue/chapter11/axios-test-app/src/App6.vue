e76f1c51bd8f6d32402f2b172bcc0fec2993ab01
rlatneorp <rlatneorp@gmail.com>
2024-06-03 17:45:00 +0900

    comment : 주석처리

--- 03_Vue/chapter11/axios-test-app/src/App6.vue
+++ 03_Vue/chapter11/axios-test-app/src/App6.vue
@@ -11,9 +11,11 @@ const requestAPI = async () => {
   const url = '/api/todos2';
   axios
     .get(url, { timeout: 900 })
+    // API호출에 성공했을경우
     .then((response) => {
       console.log('# 응답객체 : ', response);
     })
+    // API호출에 실패했을 경우
     .catch((e) => {
       console.log('에러==================');
       console.log(e);
