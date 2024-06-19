5d6bab345b99bdab894ab26ba1ff2f50f6ec1da5
rlatneorp <rlatneorp@gmail.com>
2024-06-19 17:25:58 +0900

    feat : static final 상수 연습

--- 05_Java/study/src/ch06/sec10/exam01/CalculatorExample.java
+++ 05_Java/study/src/ch06/sec10/exam01/CalculatorExample.java
@@ -2,6 +2,12 @@ package ch06.sec10.exam01;
 
 public class CalculatorExample {
     public static void main(String[] args) {
+//        static 필드와 메서드는 클래스명으로 접근한다
+
+
+//        이렇게도 쓸 수 있지만 굳이 안 쓴다
+//        Calculator calc = new Calculator();
+
         double result1 = 10 * 10 * Calculator.pi;
         int result2 = Calculator.plus(10, 5);
         int result3 = Calculator.minus(10, 5);
