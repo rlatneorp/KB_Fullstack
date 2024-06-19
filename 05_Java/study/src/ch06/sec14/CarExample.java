b63f6b0f9d7cd80c9a182a443547376e4f0ed69d
rlatneorp <rlatneorp@gmail.com>
2024-06-19 17:45:58 +0900

    comment : 주석

--- 05_Java/study/src/ch06/sec14/CarExample.java
+++ 05_Java/study/src/ch06/sec14/CarExample.java
@@ -4,6 +4,8 @@ public class CarExample {
     public static void main(String[] args) {
         Car myCar = new Car();
 
+        // 잘못된 값이 들어갔을 경우 set 메소드에서 0으로 처리해준다
+        // 사용자들은 해당 로직을 알 필요가 없으므로 캡슐화해줌
         myCar.setSpeed(-50);
         System.out.println("현재 속도: " + myCar.getSpeed());
 
