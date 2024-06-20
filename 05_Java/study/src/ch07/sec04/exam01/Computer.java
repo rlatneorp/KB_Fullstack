8d9bc7ac82295648ed8de660dc1cb36e32670838
rlatneorp <rlatneorp@gmail.com>
2024-06-20 10:49:34 +0900

    feat : 오버라이드

---
+++ 05_Java/study/src/ch07/sec04/exam01/Computer.java
@@ -0,0 +1,10 @@
+package ch07.sec04.exam01;
+
+public class Computer extends Calculator{
+    // 메서드 오버라이딩
+
+    @Override
+    public double areaCircle(double r) {
+        return super.areaCircle(r);
+    }
+}
