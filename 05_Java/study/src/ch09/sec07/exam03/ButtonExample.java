46018dd47044fdbfe994be0485368bbee952c307
rlatneorp <rlatneorp@gmail.com>
2024-06-25 16:19:39 +0900

    comment : 주석

--- 05_Java/study/src/ch09/sec07/exam03/ButtonExample.java
+++ 05_Java/study/src/ch09/sec07/exam03/ButtonExample.java
@@ -4,7 +4,8 @@ public class ButtonExample {
     //Ok 버튼 객체 생성
     public static void main(String[] args) {
         Button btnOk = new Button();
-
+        
+        // 기존에 OkListener 클래스로 구현해줬던 코드를 바로 익명 객체로 주입
         // Ok버튼 객체에 clicklistener 구현 객체 주입
         btnOk.setClickListener(new Button.ClickListener() {
 
@@ -19,7 +20,7 @@ public class ButtonExample {
 
         // Cancel 버튼 객체 생성
         Button btnCancel = new Button();
-
+        // 기존에 CancelListener 클래스로 구현해줬던 코드를 바로 익명 객체로 주입
         // Cancel 바튼 객체에 ClickListener 구현 객체 주입
         btnCancel.setClickListener(new Button.ClickListener() {
 
