d3f605bada8d7e5c2180795d6cfe17904005f7f5
rlatneorp <rlatneorp@gmail.com>
2024-06-27 15:09:26 +0900

    feat : class 이름 알기

---
+++ 05_Java/study/src/ch12/sec11/exam01/GetClassExample.java
@@ -0,0 +1,17 @@
+package ch12.sec11.exam01;
+
+public class GetClassExample {
+    public static void main(String[] args) throws Exception {
+        //how1 , class static 필드 이용
+        Class clazz = Car.class;
+        //how2 , 클래스 문자열 이용
+        //Class clazz = Class.forName("ch12.sec11.exam01.Car");
+        //how3, 인스턴스를 이용
+//        Car car = new Car();
+//        Class clazz = car.getClass();
+
+        System.out.println("패키지: " + clazz.getPackage().getName());
+        System.out.println("클래스 간단 이름: " + clazz.getSimpleName());
+        System.out.println("클래스 전체 이름: " + clazz.getName());
+    }
+}
