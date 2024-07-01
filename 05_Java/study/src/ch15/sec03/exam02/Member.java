c718752802cff4dc3a367adfb4e82ffbe931397f
rlatneorp <rlatneorp@gmail.com>
2024-07-01 17:50:03 +0900

    comment : 주석

--- 05_Java/study/src/ch15/sec03/exam02/Member.java
+++ 05_Java/study/src/ch15/sec03/exam02/Member.java
@@ -9,15 +9,14 @@ public class Member {
 
     // hashCode 재정의
     @Override
-    public int hashCode()
-    {
+    public int hashCode(){
         return name.hashCode() + age;
     }
 
     // equals 재정의
     @Override
-    public boolean equals(Object obj)
-    {
+    public boolean equals(Object obj){
+//        obj가 Member 타입일 경우 target이란 이름의 객체로 다운캐스팅해줌
         if(obj instanceof Member target) {
             return target.name.equals(name) && (target.age==age) ;
         } else {
