f2580dbc7e96800afdead307ad0150f5de9b31cc
rlatneorp <rlatneorp@gmail.com>
2024-07-03 17:00:18 +0900

    comment : 주석

--- 05_Java/study/src/ch17/sec05/FilteringExample.java
+++ 05_Java/study/src/ch17/sec05/FilteringExample.java
@@ -26,6 +26,7 @@ public class FilteringExample {
                 .forEach(n -> System.out.println(n));
         System.out.println();
 
+        // distinct()와 startsWith() 모두 사용
         // 중복 요소를 먼저 제거하고,신으로 시작하는 요소만 필터링
         list.stream()
                 .distinct()
