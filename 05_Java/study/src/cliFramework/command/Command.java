5531f6dbc7503b410640f9ac4e6990a5bba450b6
rlatneorp <rlatneorp@gmail.com>
2024-07-03 17:50:20 +0900

    comment : cli 주석

--- 05_Java/study/src/cliFramework/command/Command.java
+++ 05_Java/study/src/cliFramework/command/Command.java
@@ -1,5 +1,6 @@
 package cliFramework.command;
 
 public interface Command {
+    // 추상 메서드이므로 재정의 필요
     void execute();
 }
--- 05_Java/study/src/cliFramework/command/InitScoresCommand.java
+++ 05_Java/study/src/cliFramework/command/InitScoresCommand.java
@@ -8,7 +8,9 @@ public class InitScoresCommand implements Command {
 
     @Override
     public void execute() {
+        // getInt를 사용해서 사용자한테 학생수를 입력받음
         int studentNum = Input.getInt("학생수> ");
+        // studentNum과 점수 배열의 크기를 할당
         studentScores.setStudentNum(studentNum);
     }
 }
