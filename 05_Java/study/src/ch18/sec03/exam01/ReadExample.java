fbee2d237f943271d078090256918d812a31aabe
rlatneorp <rlatneorp@gmail.com>
2024-07-08 12:19:27 +0900

    comment : FileInputStream 주석

--- 05_Java/study/src/ch18/sec03/exam01/ReadExample.java
+++ 05_Java/study/src/ch18/sec03/exam01/ReadExample.java
@@ -9,13 +9,17 @@ public class ReadExample {
     public static void main(String[] args) {
         try (InputStream is = new FileInputStream("C:/Temp/test1.db")){
             while (true) {
-                int data = is.read(); // 1byte씩 읽기
-                if(data == -1) break; // 파일 끝에 도달했을 경우
+//                파일을 1byte씩 읽어온다
+                int data = is.read();
+//               바이트를 모두 다 읽어서 더이상 읽을 수 없는 경우 data가 -1을 리턴
+                if(data == -1) break; //
                 System.out.println(data);
             }
         } catch (FileNotFoundException e) {
+//            파일을 제대로 찾지 못했을 경우의 예외처리
             e.printStackTrace();
         } catch (IOException e) {
+//            입출력이 제대로 동작하지 않았을 경우 예외처리
             e.printStackTrace();
         }
     }
