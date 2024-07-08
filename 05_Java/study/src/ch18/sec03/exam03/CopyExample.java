a316f83667f8819531d0cecab51e3852420633f2
rlatneorp <rlatneorp@gmail.com>
2024-07-08 12:41:49 +0900

    comment : copy 주석

--- 05_Java/study/src/ch18/sec03/exam03/CopyExample.java
+++ 05_Java/study/src/ch18/sec03/exam03/CopyExample.java
@@ -12,16 +12,17 @@ public class CopyExample {
 
         try (
                 InputStream is = new FileInputStream(originFileName);
-                OutputStream os = new FileOutputStream(targetFileName);
-        ){
+                OutputStream os = new FileOutputStream(targetFileName);)
+        {
             byte[] data = new byte[1024]; // 배열 버퍼 생성
             while (true) {
-                int num = is.read(data); // 최대 1024바이트 읽기
+                int num = is.read(data); // 최대 배열의 크기인 1024바이트 읽기
                 if (num == -1) break;    // 파일을 다 읽으면 while문 종료
-                os.write(data, 0, num);// 읽은 데이터 파일에 쓰기
+//                num만큼 데이터 파일 쓰기
+                os.write(data, 0, num);//
             }
 
-            os.flush(); // 내부 버퍼 잔류 바이트를 출력하고 버퍼를 비움
+            os.flush(); // 내부 버퍼에 있는 바이트 출력하고 비우기
             System.out.println("복사가 잘 되었습니다.");
         } catch (Exception e) {
             e.printStackTrace();
