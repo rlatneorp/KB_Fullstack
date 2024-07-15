387d15ae08cbf250be51b03c6a618af7b9a5cfe8
rlatneorp <rlatneorp@gmail.com>
2024-07-15 17:16:14 +0900

    refactor : 테스트코드 수정

--- 06_SQL/jdbc_ex/src/test/java/org/scoula/jdbc_ex/test/ConnectionTest.java
+++ 06_SQL/jdbc_ex/src/test/java/org/scoula/jdbc_ex/test/ConnectionTest.java
@@ -2,6 +2,7 @@ package org.scoula.jdbc_ex.test;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.scoula.jdbc_ex.common.JDBCUtil;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
@@ -25,5 +26,15 @@ public class ConnectionTest {
         System.out.println("DB 연결 성공");
         // 데이터베이스 연결 종료
         conn.close();
+    }
+    
+    // 위 테스트 함수와는 다르게 JDBCUtil 부분에 연결을 모듈화함
+    // 재사용성과 유지보수하기 좋아짐
+    @Test
+    @DisplayName("jdbc_ex에 접속한다.(자동 닫기)")
+    void getConnection() throws SQLException {
+        try(Connection conn = JDBCUtil.getConnection()) {
+            System.out.println("DB 연결 성공");
+        }
     }
 }
