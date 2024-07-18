8f286185df4a29eb48f2d7cbe6c2a0230a831c21
rlatneorp <rlatneorp@gmail.com>
2024-07-18 15:40:28 +0900

    comment : 주석

--- 05_Java/StudentStore/src/main/java/org/scoula/studentscore/StudentScoreApp.java
+++ 05_Java/StudentStore/src/main/java/org/scoula/studentscore/StudentScoreApp.java
@@ -5,9 +5,12 @@ import org.scoula.lib.cli.ui.Menu;
 import org.scoula.lib.cli.ui.MenuItem;
 import org.scoula.studentscore.service.StudentScoreService;
 
+//커스터마이징하는 부분만 App 클래스에서 추출하여 자식 클래스 생성
 public class StudentScoreApp extends App {
+//    service 클래스 내에 메뉴 관련 메소드들을 모두 넣고 서비스 객체 생성
     StudentScoreService service = new StudentScoreService();
 
+//    메소드 참조로 서비스 객체 내에 있는 메소드 접근
     @Override
     public void createMenu(Menu menu) {
         menu.add(new MenuItem("학생수", service::initScores));
