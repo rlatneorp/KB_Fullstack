7a64ab9c441142a1a665b08f7971693435c98c37
rlatneorp <rlatneorp@gmail.com>
2024-07-25 15:50:55 +0900

    feat : servlet 예시

--- 10_JSP,Servlet/ex02/src/main/java/org/scoula/ex02/HelloServlet.java
+++ 10_JSP,Servlet/ex02/src/main/java/org/scoula/ex02/HelloServlet.java
@@ -4,24 +4,13 @@ import java.io.*;
 import javax.servlet.http.*;
 import javax.servlet.annotation.*;
 
-// @WebServlet(name = "helloServlet", value = "/hello-servlet")
+ @WebServlet(name = "MyServlet", urlPatterns = {"/xxx", "/yyy"})
 public class HelloServlet extends HttpServlet {
-    private String message;
-
-    public void init() {
-        message = "Hello World!";
-    }
 
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-        response.setContentType("text/html");
-
-        // Hello
+        System.out.println("HelloServlet 요청");
         PrintWriter out = response.getWriter();
-        out.println("<html><body>");
-        out.println("<h1>" + message + "</h1>");
-        out.println("</body></html>");
+        out.println("<h1>Hello World!</h1>");
     }
 
-    public void destroy() {
-    }
 }
--- 10_JSP,Servlet/ex02/src/main/webapp/index.jsp
+++ 10_JSP,Servlet/ex02/src/main/webapp/index.jsp
@@ -8,6 +8,7 @@
 <h1><%= "Hello World!" %>
 </h1>
 <br/>
-<a href="hello-servlet">Hello Servlet</a>
+<a href="xxx">Hello Servlet</a>
+<a href="yyy">Hello Servlet</a>
 </body>
 </html>
