207bf806246358f4126cfa19a79d549a4a30647c
rlatneorp <rlatneorp@gmail.com>
2024-08-06 16:31:29 +0900

    comment : db연결 주석

--- 11_Spring/ex04/src/main/java/org/scoula/config/RootConfig.java
+++ 11_Spring/ex04/src/main/java/org/scoula/config/RootConfig.java
@@ -6,12 +6,14 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
-
 import javax.sql.DataSource;
 
 @Configuration
+//어떤 경로에서 property를 가져올지 설정(classpath를 붙여줘야 프로젝트의 루트로 접근)
+//그냥 /를 사용하면 webapp 폴더가 루트가 된다
 @PropertySource({"classpath:/application.properties"})
 public class RootConfig {
+//    application.properties에서 값을 읽어와서 변수에 주입
     @Value("${jdbc.driver}") String driver;
     @Value("${jdbc.url}") String url;
     @Value("${jdbc.username}") String username;
@@ -21,11 +23,13 @@ public class RootConfig {
     public DataSource dataSource() {
         HikariConfig config = new HikariConfig();
 
+//        설정 객체에 데이터베이스 연결 정보를 설정
         config.setDriverClassName(driver);
         config.setJdbcUrl(url);
         config.setUsername(username);
         config.setPassword(password);
 
+//        HikariDataSource 객체 생성 후 설정을 적용
         HikariDataSource dataSource = new HikariDataSource(config);
         return dataSource;
     }
--- 11_Spring/ex04/src/main/java/org/scoula/config/WebConfig.java
+++ 11_Spring/ex04/src/main/java/org/scoula/config/WebConfig.java
@@ -4,7 +4,6 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.filter.CharacterEncodingFilter;
 import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
-
 import javax.servlet.Filter;
 import javax.servlet.MultipartConfigElement;
 import javax.servlet.ServletRegistration;
