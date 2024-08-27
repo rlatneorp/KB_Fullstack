dce5e6a21f4f8dfda3c0d799ec55303c884c38fc
rlatneorp <rlatneorp@gmail.com>
2024-08-27 11:37:00 +0900

    feat : vue로 회원가입 하기

--- 12_Vue+Spring/scoula/frontend/src/pages/auth/JoinPage.vue
+++ 12_Vue+Spring/scoula/frontend/src/pages/auth/JoinPage.vue
@@ -1,4 +1,53 @@
-<script setup></script>
-<template>
-  <h1>첫번째 페이지</h1>
-</template>
+<script setup>
+import { reactive, ref } from 'vue';
+import { useRouter } from 'vue-router';
+import authApi from '@/api/authApi';
+
+const router = useRouter();
+const avatar = ref(null);
+const checkError = ref('');
+
+const member = reactive({
+  // 테스트용 초기화
+  username: 'hong',
+  email: 'hong@gmail.com',
+  password: '12',
+  password2: '12',
+  avatar: null,
+});
+const disableSubmit = ref(true);
+// username 중복 체크
+const checkUsername = async () => {
+  if (!member.username) {
+    return alert('사용자 ID를 입력하세요.');
+  }
+  disableSubmit.value = await authApi.checkUsername(member.username);
+  console.log(disableSubmit.value, typeof disableSubmit.value);
+  checkError.value = disableSubmit.value
+    ? '이미 사용중인 ID입니다.'
+    : '사용가능한 ID입니다.';
+};
+// username 입력 핸들러
+const changeUsername = () => {
+  disableSubmit.value = true;
+  if (member.username) {
+    checkError.value = 'ID 중복 체크를 하셔야 합니다.';
+  } else {
+    checkError.value = '';
+  }
+};
+const join = async () => {
+  if (member.password != member.password2) {
+    return alert('비밀번호가 일치하지 않습니다.');
+  }
+  if (avatar.value.files.length > 0) {
+    member.avatar = avatar.value.files[0];
+  }
+  try {
+    await authApi.create(member); // 회원가입
+    router.push({ name: 'home' }); // 회원 가입 성공 시, 첫 페이지로 이동 또는 로그인 페이지로 이동
+  } catch (e) {
+    console.error(e);
+  }
+};
+</script>
