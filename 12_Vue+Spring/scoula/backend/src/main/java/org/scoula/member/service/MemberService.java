40f612138c70ac78dd075476c3a8fa7f53ebc659
rlatneorp <rlatneorp@gmail.com>
2024-08-27 14:28:05 +0900

    feat : mapper

--- 12_Vue+Spring/scoula/backend/src/main/java/org/scoula/member/service/MemberService.java
+++ 12_Vue+Spring/scoula/backend/src/main/java/org/scoula/member/service/MemberService.java
@@ -4,9 +4,12 @@ import org.scoula.member.dto.MemberDTO;
 import org.scoula.member.dto.MemberJoinDTO;
 public interface MemberService {
 
+//    주어진 사용자 이름이 중복되는지 체크
     boolean checkDuplicate(String username);
 
+//    주어진 사용자 이름으로 회원 정보를 가져옴
     MemberDTO get(String username);
 
+//    회원 가입 처리, MemberJoinDTO를 받아 회원 정보를 저장하고 저장된 회원 정보 반환
     MemberDTO join(MemberJoinDTO member);
 }
